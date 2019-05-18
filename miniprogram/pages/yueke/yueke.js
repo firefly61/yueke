da61d006d1276f7d3ea87659d7e24c7a76dc52ca
刘颖 <nicoledeyx@163.com>
2019-05-18 16:33:46 +0800

    课程列表

--- miniprogram/pages/yueke/yueke.js
+++ miniprogram/pages/yueke/yueke.js
@@ -4,6 +4,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-    text: 'show me'
+    courseList: [
+      {
+        index: 1,
+        teacher: {
+          name: '小默',
+          photo: 'https://via.placeholder.com/150'
+        },
+        courseName: '流瑜伽',
+        time: '14:00 - 15:00',
+        nums: 12,
+        total: 15,
+        stars: 2,
+        state: 1
+      },
+      {
+        index: 2,
+        teacher: {
+          name: '小默',
+          photo: 'https://via.placeholder.com/150'
+        },
+        courseName: '阴瑜伽',
+        time: '18:30 - 19:30',
+        nums: 12,
+        total: 15,
+        stars: 2,
+        state: 1
+      }
+    ]
   }
 })
